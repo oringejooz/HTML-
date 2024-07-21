a5a4aeddfeadcd120488310436b4d2a6e53d1d33
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-07-21 17:46:39 +0530

    Changes to be committed: 	modified:   JavaScript/Arrays/arraymethods.html 	modified:   JavaScript/Arrays/arraymethods.js

---
+++ JavaScript/Arrays/arraymethods.js
@@ -0,0 +1,35 @@
+/*
+
+Array Methods
+
+Types 
+    That change the existing array
+    Do not change the existing array but create a new one
+
+
+    push() Add at end
+    pop() - delete item from end and returns the deleted item
+    toString() - converts to string
+    arr.concat(a1,a2,...) - joins multiple arrays and returns result
+    shift() - deleted from start and return
+    unshift() - add to start
+    slice(start,end)
+    splice() - change original array (add,remove,replace)
+    */
+
+let food = ["pizza","laphing","chicken"];
+console.log(food);
+food.push("laddoo","khichdi");
+console.log(food);
+
+let delItem = food.pop();
+console.log(food);
+console.log(food.toString());
+let a2 =[1,2,3];
+console.log(food.concat(a2));
+let a3 =["Sa","Re","Ga"];
+console.log(food.concat(a2,a3));
+
+a2.unshift('-1');
+a2.shift()
+a2.unshift(0)
