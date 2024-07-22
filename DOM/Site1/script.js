3c2c1ecf5eec1b8bf03871e97acdf98b3278791c
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-07-22 10:17:57 +0530

    Changes to be committed: 	new file:   DOM/Site1/index.html 	new file:   DOM/Site1/script.js 	new file:   DOM/Site1/style.css

---
+++ DOM/Site1/script.js
@@ -0,0 +1,17 @@
+/* 
+
+A window object represents an open window in a browser. It is browser's object not
+JavaScript's and is automatically created by the browser
+
+it is a global object with lots of properties and methods
+
+*/
+// console.log(window);
+
+// console.log("Hello");
+// window.console.log("Hello");
+
+// alert("Window Object");
+// window.alert("Hi")
+console.dir(window.document) //Prints methods and properties of special objects (DOM Objects)
+console.log(window.document)//Prints the html code
